8e57ad6f73b1ccd985ae48777675116b0927632b
Robert Horvick <robert.horvick@gmail.com>
2013-03-21 15:28:10 -0400

    Moving samples around, adding HelloWorld sample from Readme

--- src/Samples/HelloWorld/Properties/AssemblyInfo.cs
+++ src/Samples/HelloWorld/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PingPong")]
+[assembly: AssemblyTitle("HelloWorld")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("PingPong")]
+[assembly: AssemblyProduct("HelloWorld")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
