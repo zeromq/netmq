78b6732af62675b30e9699436be65f38bf6c9812
Christophe Juniet <c.juniet@gmail.com>
2013-06-20 14:48:16 +0200

    BugFix: NetMQ.Proxy throws on Start
    NetMQ.Proxy was broken because ZMQ.Poll throws a null ref when called
    with an infinite timeout.
    Add a unit test on ZMQ.Poll(-1) and another one on NetMQ.Proxy.

--- src/NetMQ.Tests/ProxyTests.cs
+++ src/NetMQ.Tests/ProxyTests.cs
@@ -1,11 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Threading;
+using System.Threading.Tasks;
+using NetMQ.zmq;
+using NUnit.Framework;
 
 namespace NetMQ.Tests
 {
-	class ProxyTests
-	{
-	}
+    [TestFixture]
+    public class ProxyTests
+    {
+        [Test]
+        public void TestInfinitePolling()
+        {
+            using (var context = NetMQContext.Create())
+            {
+                using (var socket = context.CreateDealerSocket())
+                {
+                    socket.Bind("inproc://test");
+                    var items = new PollItem[1];
+                    items[0] = new PollItem(socket.SocketHandle, PollEvents.PollIn);
+
+                    Task.Factory.StartNew(() =>
+                    {
+                        Thread.Sleep(500);
+                        using (var monitor = context.CreateDealerSocket())
+                        {
+                            monitor.Connect("inproc://test");
+                            monitor.Send("ping");
+                        }
+                    });
+
+                    Assert.DoesNotThrow(() => ZMQ.Poll(items, -1));
+                    Assert.AreEqual(socket.ReceiveString(SendReceiveOptions.DontWait), "ping");
+                }
+            }
+        }
+
+        [Test]
+        public void TestProxySendAndReceive()
+        {
+            using (var ctx = NetMQContext.Create())
+            {
+                using (var front = ctx.CreateRouterSocket())
+                {
+                    front.Bind("inproc://frontend");
+
+                    using (var back = ctx.CreateDealerSocket())
+                    {
+                        back.Bind("inproc://backend");
+
+                        var proxy = new Proxy(front, back, null);
+                        Task.Factory.StartNew(proxy.Start);
+
+                        using (var client = ctx.CreateRequestSocket())
+                        {
+                            client.Connect("inproc://frontend");
+
+                            using (var server = ctx.CreateResponseSocket())
+                            {
+                                server.Connect("inproc://backend");
+
+                                client.Send("hello");
+                                Assert.AreEqual("hello", server.ReceiveString());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
