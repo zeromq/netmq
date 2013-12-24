ba2ecef69d8ad7a29ac2c1159230ae7bc8b30b65
Chris Perks <chris.perks@lastexit.tv>
2013-12-24 00:43:09 +0000

    Added 'Extended Request Reply' sample as per zguide Shared Queue implementation
    as per http://zguide.zeromq.org/page:all#toc39

---
+++ src/Samples/Load Balancing Pattern/Extended Request Reply/RequestReplyClient/RequestReplyClient.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NetMQ;
+
+namespace ExtendedRequestReply
+{
+    class RequestReplyClient
+    {
+        private const string CLIENT_ENDPOINT = "tcp://127.0.0.1:5559";
+
+        static void Main(string[] args)
+        {
+            using (var ctx = NetMQContext.Create())
+            {
+                using (var client = ctx.CreateRequestSocket())
+                {
+                    client.Connect(CLIENT_ENDPOINT);
+                    for (var i = 0; i < 10; i++)
+                    {
+                        var msg = new NetMQMessage();
+                        msg.Append("Message_" + i);
+                        client.SendMessage(msg);
+                        Console.WriteLine("Sent Message {0}", msg.Last.ConvertToString());
+                        var response = client.ReceiveMessage();
+                        Console.WriteLine("Received Message {0}", response.Last.ConvertToString());
+                    }
+
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
