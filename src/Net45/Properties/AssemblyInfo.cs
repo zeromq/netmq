9961852d1c5c317569f5da21c96f6b4f8d882ee4
Diego Duclos <diego.duclos@palmstonegames.com>
2014-01-20 15:33:05 +0100

    Add 3.5 project to the solution, fix a messup in previous commit with the AssemblyInfo.cs of the 4.5 project

---
+++ src/Net45/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NetMQ")]
+[assembly: AssemblyDescription("NetMQ is 100% C# native port of ZeroMQ. " +
+    "NetMQ is lightweight high performance messaging library. This current version is still in alpha phase and not recommended for production usage yet")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("NetMQ")]
+//[assembly: AssemblyCompany("iMatix Corporation")]
+[assembly: AssemblyProduct("NetMQ")]
+[assembly: AssemblyCopyright("Copyright © 2012 iMatix Corporation and Contributors")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("87c668fe-0277-4435-9653-beee15a587e3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
