864d9ea6de64d21c2038f2537cb90f38210d0ebd
somdoron <somdoron@gmail.com>
2014-06-14 00:55:41 +0300

    Fix issue #109, assembly is not signed

--- src/NetMQ/Properties/AssemblyInfo.cs
+++ src/NetMQ/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("NetMQ")]
 [assembly: AssemblyDescription("NetMQ is 100% C# native port of ZeroMQ. " + 
-	"NetMQ is lightweight high performance messaging library. This current version is still in alpha phase and not recommended for production usage yet")]
+	"NetMQ is lightweight high performance messaging library.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("NetMQ")]
 //[assembly: AssemblyCompany("iMatix Corporation")]
