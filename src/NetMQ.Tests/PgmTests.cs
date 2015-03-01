e2fae6caeea054b1b4355a014cc8002816c1b6ce
Drew Noakes <git@drewnoakes.com>
2015-03-01 17:57:40 +0000

    Simplify format of PgmTests.

--- src/NetMQ.Tests/PgmTests.cs
+++ src/NetMQ.Tests/PgmTests.cs
@@ -14,61 +14,46 @@ namespace NetMQ.Tests
     //
     // Note: The 224.0.0.1 is the IPv4 All Hosts multicast group which addresses all hosts on the same network segment.
 
-
     [TestFixture(Category = "PGM")]
     public class PgmTests
     {
         [Test]
         public void SimplePubSub()
         {
-            using (NetMQContext context = NetMQContext.Create())
+            using (var context = NetMQContext.Create())
+            using (var pub = context.CreatePublisherSocket())
+            using (var sub = context.CreateSubscriberSocket())
             {
-                using (var pub = context.CreatePublisherSocket())
-                {
-                    pub.Connect("pgm://224.0.0.1:5555");
+                pub.Connect("pgm://224.0.0.1:5555");
+                sub.Bind("pgm://224.0.0.1:5555");
 
-                    using (var sub = context.CreateSubscriberSocket())
-                    {
-                        sub.Bind("pgm://224.0.0.1:5555");
+                sub.Subscribe("");
 
-                        sub.Subscribe("");
+                pub.Send("Hi");
 
-                        pub.Send("Hi");
-
-                        bool more;
-                        string message = sub.ReceiveString(out more);
-
-                        Assert.IsFalse(more);
-                        Assert.AreEqual("Hi", message);
-                    }
-                }
+                bool more;
+                Assert.AreEqual("Hi", sub.ReceiveString(out more));
+                Assert.IsFalse(more);
             }
         }
 
         [Test]
         public void BindBothSockets()
         {
-            using (NetMQContext context = NetMQContext.Create())
+            using (var context = NetMQContext.Create())
+            using (var pub = context.CreatePublisherSocket())
+            using (var sub = context.CreateSubscriberSocket())
             {
-                using (var pub = context.CreatePublisherSocket())
-                {
-                    pub.Bind("pgm://224.0.0.1:5555");
+                pub.Bind("pgm://224.0.0.1:5555");
+                sub.Bind("pgm://224.0.0.1:5555");
 
-                    using (var sub = context.CreateSubscriberSocket())
-                    {
-                        sub.Bind("pgm://224.0.0.1:5555");
+                sub.Subscribe("");
 
-                        sub.Subscribe("");
+                pub.Send("Hi");
 
-                        pub.Send("Hi");
-
-                        bool more;
-                        string message = sub.ReceiveString(out more);
-
-                        Assert.IsFalse(more);
-                        Assert.AreEqual("Hi", message);
-                    }
-                }
+                bool more;
+                Assert.AreEqual("Hi", sub.ReceiveString(out more));
+                Assert.IsFalse(more);
             }
         }
 
@@ -76,26 +61,19 @@ namespace NetMQ.Tests
         public void ConnectBothSockets()
         {
             using (NetMQContext context = NetMQContext.Create())
+            using (var pub = context.CreatePublisherSocket())
+            using (var sub = context.CreateSubscriberSocket())
             {
-                using (var pub = context.CreatePublisherSocket())
-                {
-                    pub.Connect("pgm://224.0.0.1:5555");
+                pub.Connect("pgm://224.0.0.1:5555");
+                sub.Connect("pgm://224.0.0.1:5555");
 
-                    using (var sub = context.CreateSubscriberSocket())
-                    {
-                        sub.Connect("pgm://224.0.0.1:5555");
+                sub.Subscribe("");
 
-                        sub.Subscribe("");
+                pub.Send("Hi");
 
-                        pub.Send("Hi");
-
-                        bool more;
-                        string message = sub.ReceiveString(out more);
-
-                        Assert.IsFalse(more);
-                        Assert.AreEqual("Hi", message);
-                    }
-                }
+                bool more;
+                Assert.AreEqual("Hi", sub.ReceiveString(out more));
+                Assert.IsFalse(more);
             }
         }
 
@@ -109,27 +87,20 @@ namespace NetMQ.Tests
                                  select addr.ToString()
                     ).FirstOrDefault();
 
-            using (NetMQContext context = NetMQContext.Create())
+            using (var context = NetMQContext.Create())
+            using (var pub = context.CreatePublisherSocket())
+            using (var sub = context.CreateSubscriberSocket())
             {
-                using (var pub = context.CreatePublisherSocket())
-                {
-                    pub.Connect(string.Format("pgm://{0};224.0.0.1:5555", ip));
+                pub.Connect(string.Format("pgm://{0};224.0.0.1:5555", ip));
+                sub.Bind(string.Format("pgm://{0};224.0.0.1:5555", ip));
 
-                    using (var sub = context.CreateSubscriberSocket())
-                    {
-                        sub.Bind(string.Format("pgm://{0};224.0.0.1:5555", ip));
+                sub.Subscribe("");
 
-                        sub.Subscribe("");
+                pub.Send("Hi");
 
-                        pub.Send("Hi");
-
-                        bool more;
-                        string message = sub.ReceiveString(out more);
-
-                        Assert.IsFalse(more);
-                        Assert.AreEqual("Hi", message);
-                    }
-                }
+                bool more;
+                Assert.AreEqual("Hi", sub.ReceiveString(out more));
+                Assert.IsFalse(more);
             }
         }
 
@@ -139,117 +110,92 @@ namespace NetMQ.Tests
             const int MegaBit = 1024;
             const int MegaByte = 1024;
 
-            using (NetMQContext context = NetMQContext.Create())
+            using (var context = NetMQContext.Create())
+            using (var pub = context.CreatePublisherSocket())
+            using (var sub = context.CreateSubscriberSocket())
             {
-                using (var pub = context.CreatePublisherSocket())
-                {
-                    pub.Options.MulticastHops = 2;
-                    pub.Options.MulticastRate = 40 * MegaBit; // 40 megabit
-                    pub.Options.MulticastRecoveryInterval = TimeSpan.FromMinutes(10);
-                    pub.Options.SendBuffer = MegaByte * 10; // 10 megabyte
+                pub.Options.MulticastHops = 2;
+                pub.Options.MulticastRate = 40*MegaBit; // 40 megabit
+                pub.Options.MulticastRecoveryInterval = TimeSpan.FromMinutes(10);
+                pub.Options.SendBuffer = MegaByte*10; // 10 megabyte
 
-                    pub.Connect("pgm://224.0.0.1:5555");
+                pub.Connect("pgm://224.0.0.1:5555");
 
-                    using (var sub = context.CreateSubscriberSocket())
-                    {
-                        sub.Options.ReceiveBuffer = MegaByte * 10;
-                        sub.Bind("pgm://224.0.0.1:5555");
+                sub.Options.ReceiveBuffer = MegaByte*10;
+                sub.Bind("pgm://224.0.0.1:5555");
 
-                        sub.Subscribe("");
+                sub.Subscribe("");
 
-                        pub.Send("Hi");
+                pub.Send("Hi");
 
-                        bool more;
-                        string message = sub.ReceiveString(out more);
+                bool more;
+                Assert.AreEqual("Hi", sub.ReceiveString(out more));
+                Assert.IsFalse(more);
 
-                        Assert.IsFalse(more);
-                        Assert.AreEqual("Hi", message);
-
-                        Assert.AreEqual(2, pub.Options.MulticastHops);
-                        Assert.AreEqual(40 * MegaBit, pub.Options.MulticastRate);
-                        Assert.AreEqual(TimeSpan.FromMinutes(10), pub.Options.MulticastRecoveryInterval);
-                        Assert.AreEqual(MegaByte * 10, pub.Options.SendBuffer);
-                        Assert.AreEqual(MegaByte * 10, sub.Options.ReceiveBuffer);
-                    }
-                }
+                Assert.AreEqual(2, pub.Options.MulticastHops);
+                Assert.AreEqual(40*MegaBit, pub.Options.MulticastRate);
+                Assert.AreEqual(TimeSpan.FromMinutes(10), pub.Options.MulticastRecoveryInterval);
+                Assert.AreEqual(MegaByte*10, pub.Options.SendBuffer);
+                Assert.AreEqual(MegaByte*10, sub.Options.ReceiveBuffer);
             }
         }
 
         [Test]
         public void TwoSubscribers()
         {
-            using (NetMQContext context = NetMQContext.Create())
+            using (var context = NetMQContext.Create())
+            using (var pub = context.CreatePublisherSocket())
+            using (var sub = context.CreateSubscriberSocket())
+            using (var sub2 = context.CreateSubscriberSocket())
             {
-                using (var pub = context.CreatePublisherSocket())
-                {
-                    pub.Connect("pgm://224.0.0.1:5555");
+                pub.Connect("pgm://224.0.0.1:5555");
+                sub.Bind("pgm://224.0.0.1:5555");
+                sub2.Bind("pgm://224.0.0.1:5555");
 
-                    using (var sub = context.CreateSubscriberSocket())
-                    {
-                        using (var sub2 = context.CreateSubscriberSocket())
-                        {
-                            sub.Bind("pgm://224.0.0.1:5555");
-                            sub2.Bind("pgm://224.0.0.1:5555");
+                sub.Subscribe("");
+                sub2.Subscribe("");
 
-                            sub.Subscribe("");
-                            sub2.Subscribe("");
+                pub.Send("Hi");
 
-                            pub.Send("Hi");
+                bool more;
+                Assert.AreEqual("Hi", sub.ReceiveString(out more));
+                Assert.IsFalse(more);
 
-                            bool more;
-                            string message = sub.ReceiveString(out more);
-
-                            Assert.IsFalse(more);
-                            Assert.AreEqual("Hi", message);
-
-                            message = sub2.ReceiveString(out more);
-
-                            Assert.IsFalse(more);
-                            Assert.AreEqual("Hi", message);
-                        }
-                    }
-                }
+                Assert.AreEqual("Hi", sub2.ReceiveString(out more));
+                Assert.IsFalse(more);
             }
         }
 
         [Test]
         public void TwoPublishers()
         {
-            using (NetMQContext context = NetMQContext.Create())
+            using (var context = NetMQContext.Create())
+            using (var pub = context.CreatePublisherSocket())
+            using (var pub2 = context.CreatePublisherSocket())
+            using (var sub = context.CreateSubscriberSocket())
             {
-                using (var pub = context.CreatePublisherSocket())
-                {
-                    pub.Connect("pgm://224.0.0.1:5555");
-                    using (var pub2 = context.CreatePublisherSocket())
-                    {
-                        pub2.Connect("pgm://224.0.0.1:5555");
+                pub.Connect("pgm://224.0.0.1:5555");
+                pub2.Connect("pgm://224.0.0.1:5555");
+                sub.Bind("pgm://224.0.0.1:5555");
 
-                        using (var sub = context.CreateSubscriberSocket())
-                        {
-                            sub.Bind("pgm://224.0.0.1:5555");
+                sub.Subscribe("");
 
-                            sub.Subscribe("");
+                pub.Send("Hi");
 
-                            pub.Send("Hi");
+                bool more;
+                string message = sub.ReceiveString(out more);
 
-                            bool more;
-                            string message = sub.ReceiveString(out more);
+                Assert.IsFalse(more);
+                Assert.AreEqual("Hi", message);
 
-                            Assert.IsFalse(more);
-                            Assert.AreEqual("Hi", message);
+                pub2.Send("Hi2");
 
-                            pub2.Send("Hi2");
+                message = sub.ReceiveString(out more);
 
-                            message = sub.ReceiveString(out more);
-
-                            Assert.IsFalse(more);
-                            Assert.AreEqual("Hi2", message);
-                        }
-                    }
-                }
+                Assert.IsFalse(more);
+                Assert.AreEqual("Hi2", message);
             }
         }
-
 
         [Test]
         public void Sending1000Messages()
@@ -258,56 +204,50 @@ namespace NetMQ.Tests
 
             int count = 0;
 
-            ManualResetEvent subReady = new ManualResetEvent(false);
+            var subReady = new ManualResetEvent(false);
 
             Task subTask = Task.Factory.StartNew(() =>
-                            {
-                                using (NetMQContext context = NetMQContext.Create())
-                                {
-                                    using (var sub = context.CreateSubscriberSocket())
-                                    {
-                                        sub.Bind("pgm://224.0.0.1:5555");
-                                        sub.Subscribe("");
+            {
+                using (var context = NetMQContext.Create())
+                using (var sub = context.CreateSubscriberSocket())
+                {
+                    sub.Bind("pgm://224.0.0.1:5555");
+                    sub.Subscribe("");
 
-                                        subReady.Set();
+                    subReady.Set();
 
-                                        while (count < 1000)
-                                        {
-                                            bool more;
-                                            byte[] data = sub.Receive(out more);
+                    while (count < 1000)
+                    {
+                        bool more;
+                        byte[] data = sub.Receive(out more);
 
-                                            Assert.IsFalse(more);
-                                            int num = BitConverter.ToInt32(data, 0);
+                        Assert.IsFalse(more);
+                        int num = BitConverter.ToInt32(data, 0);
 
-                                            Assert.AreEqual(num, count);
+                        Assert.AreEqual(num, count);
 
-                                            count++;
-                                        }
-                                    }
-                                }
-                            });
+                        count++;
+                    }
+                }
+            });
 
             subReady.WaitOne();
 
             Task pubTask = Task.Factory.StartNew(() =>
-                            {
-                                using (NetMQContext context = NetMQContext.Create())
-                                {
-                                    using (var pub = context.CreatePublisherSocket())
-                                    {
-                                        pub.Connect("pgm://224.0.0.1:5555");
+            {
+                using (NetMQContext context = NetMQContext.Create())
+                using (var pub = context.CreatePublisherSocket())
+                {
+                    pub.Connect("pgm://224.0.0.1:5555");
 
-                                        for (int i = 0; i < 1000; i++)
-                                        {
-                                            pub.Send(BitConverter.GetBytes(i));
-                                        }
+                    for (int i = 0; i < 1000; i++)
+                        pub.Send(BitConverter.GetBytes(i));
 
-                                        // if we close the socket before the subscriber receives all messages subscriber
-                                        // might miss messages, lets wait another second
-                                        Thread.Sleep(1000);
-                                    }
-                                }
-                            });
+                    // if we close the socket before the subscriber receives all messages subscriber
+                    // might miss messages, lets wait another second
+                    Thread.Sleep(1000);
+                }
+            });
 
             pubTask.Wait();
             subTask.Wait();
@@ -317,43 +257,31 @@ namespace NetMQ.Tests
             Assert.AreEqual(1000, count);
         }
 
-
         [Test]
         public void LargeMessage()
         {
             using (NetMQContext context = NetMQContext.Create())
+            using (var pub = context.CreatePublisherSocket())
+            using (var sub = context.CreateSubscriberSocket())
             {
-                using (var pub = context.CreatePublisherSocket())
-                {
-                    pub.Connect("pgm://224.0.0.1:5555");
+                pub.Connect("pgm://224.0.0.1:5555");
+                sub.Bind("pgm://224.0.0.1:5555");
 
-                    using (var sub = context.CreateSubscriberSocket())
-                    {
-                        sub.Bind("pgm://224.0.0.1:5555");
+                sub.Subscribe("");
 
-                        sub.Subscribe("");
+                var data = new byte[3200]; // this should be at least 3 packets
 
-                        byte[] data = new byte[3200]; // this should be at least 3 packets
+                for (Int16 i = 0; i < 1600; i++)
+                    Array.Copy(BitConverter.GetBytes(i), 0, data, i*2, 2);
 
-                        for (Int16 i = 0; i < 1600; i++)
-                        {
-                            Array.Copy(BitConverter.GetBytes(i), 0, data, i * 2, 2);
-                        }
+                pub.Send(data);
 
-                        pub.Send(data);
-                        bool more;
-                        byte[] message = sub.Receive(out more);
+                byte[] message = sub.Receive();
 
-                        Assert.AreEqual(3200, message.Length);
+                Assert.AreEqual(3200, message.Length);
 
-                        for (Int16 i = 0; i < 1600; i++)
-                        {
-                            Int16 value = BitConverter.ToInt16(message, i * 2);
-
-                            Assert.AreEqual(i, value);
-                        }
-                    }
-                }
+                for (Int16 i = 0; i < 1600; i++)
+                    Assert.AreEqual(i, BitConverter.ToInt16(message, i*2));
             }
         }
     }
