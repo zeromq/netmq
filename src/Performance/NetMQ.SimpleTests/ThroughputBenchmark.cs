54da98e71c796ee19ce00375936adf19ee80171e
Drew Noakes <git@drewnoakes.com>
2015-03-04 00:35:32 +0000

    Reformat output from throughout performance test.

--- src/Performance/NetMQ.SimpleTests/ThroughputBenchmark.cs
+++ src/Performance/NetMQ.SimpleTests/ThroughputBenchmark.cs
@@ -6,7 +6,7 @@ namespace NetMQ.SimpleTests
 {
     internal class ThroughputBenchmark : ITest
     {
-        private static readonly int[] MessageSizes = { 8, 64, 256, 1024, 4096 };
+        private static readonly int[] s_messageSizes = { 8, 64, 256, 1024, 4096 };
 
         private const int MsgCount = 1000000;
 
@@ -17,68 +17,63 @@ namespace NetMQ.SimpleTests
 
         public void RunTest()
         {
-            var proxyPull = new Thread(ProxyPullThread);
-            var proxyPush = new Thread(ProxyPushThread);
+            Console.Out.WriteLine(" Messages: {0:#,##0}", MsgCount);
+            Console.Out.WriteLine();
+            Console.Out.WriteLine(" {0,-6} {1,10} {2,8}", "Size", "Msgs/sec", "Mb/s");
+            Console.Out.WriteLine("----------------------------");
 
-            proxyPull.Start();
-            proxyPush.Start();
+            var consumer = new Thread(ConsumerThread) { Name = "Consumer" };
+            var producer = new Thread(ProducerThread) { Name = "Producer" };
 
-            proxyPush.Join();
-            proxyPull.Join();
+            consumer.Start();
+            producer.Start();
+
+            producer.Join();
+            consumer.Join();
         }
 
-        private static void ProxyPullThread()
+        private void ConsumerThread()
         {
             using (var context = NetMQContext.Create())
             using (var socket = context.CreatePullSocket())
             {
                 socket.Bind("tcp://*:9091");
 
-                foreach (var messageSize in MessageSizes)
+                foreach (var messageSize in s_messageSizes)
                 {
-                    byte[] message = socket.Receive();
-                    //Debug.Assert(receivedBytes == messageSize, "Message length was different from expected size.");
-                    Debug.Assert(message[messageSize/2] == 0x42, "Message did not contain verification data.");
+                    var watch = Stopwatch.StartNew();
 
-                    var watch = new Stopwatch();
-                    watch.Start();
-
-                    for (int i = 1; i < MsgCount; i++)
+                    for (int i = 0; i < MsgCount; i++)
                     {
-                        message = socket.Receive();
-                        //Debug.Assert(receivedBytes == messageSize, "Message length was different from expected size.");
+                        var message = socket.Receive();
+                        Debug.Assert(message.Length == messageSize, "Message length was different from expected size.");
                         Debug.Assert(message[messageSize/2] == 0x42, "Message did not contain verification data.");
                     }
 
-                    watch.Stop();
+                    long ticks = watch.ElapsedTicks;
+                    double seconds = (double)ticks/Stopwatch.Frequency;
+                    double msgsPerSec = MsgCount/seconds;
+                    double megabitsPerSec = msgsPerSec*messageSize*8/1000000;
 
-                    long elapsedTime = watch.ElapsedTicks;
-                    long messageThroughput = MsgCount*Stopwatch.Frequency/elapsedTime;
-                    long megabitThroughput = messageThroughput*messageSize*8/1000000;
-
-                    Console.WriteLine("Message size: {0} [B]", messageSize);
-                    Console.WriteLine("Average throughput: {0} [msg/s]", messageThroughput);
-                    Console.WriteLine("Average throughput: {0} [Mb/s]", megabitThroughput);
+                    Console.Out.WriteLine(" {0,-6} {1,10:0.0} {2,8:0.00}", messageSize, msgsPerSec, megabitsPerSec);
                 }
             }
         }
 
-        private static void ProxyPushThread()
+        private void ProducerThread()
         {
             using (var context = NetMQContext.Create())
             using (var socket = context.CreatePushSocket())
             {
                 socket.Connect("tcp://127.0.0.1:9091");
 
-                foreach (var messageSize in MessageSizes)
+                foreach (var messageSize in s_messageSizes)
                 {
                     var msg = new byte[messageSize];
                     msg[messageSize/2] = 0x42;
 
                     for (int i = 0; i < MsgCount; i++)
-                    {
                         socket.Send(msg);
-                    }
                 }
             }
         }
