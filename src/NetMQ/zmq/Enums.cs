89216e70ca50157182ed1b3af5ab1e065b93c837
Drew Noakes <git@drewnoakes.com>
2015-03-10 23:44:09 +0000

    Make ContextOption enum internal.
    There's no sensible way existing client code could have used this.

--- src/NetMQ/zmq/Enums.cs
+++ src/NetMQ/zmq/Enums.cs
@@ -5,7 +5,7 @@ namespace NetMQ.zmq
     /// <summary>
     /// This enum-type is either IOThreads (1) or MaxSockets (2).
     /// </summary>
-    public enum ContextOption
+    internal enum ContextOption
     {
         IOThreads = 1,
         MaxSockets = 2
