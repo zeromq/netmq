b853ef06fd25bac0bc47d6ac01e0171a97918a30
Drew Noakes <git@drewnoakes.com>
2015-03-12 23:49:41 +0000

    Compare as enum, not string.

--- src/Samples/Brokerless Reliability (Freelance Pattern)/Model One/Freelance.ModelOne.Server/Program.cs
+++ src/Samples/Brokerless Reliability (Freelance Pattern)/Model One/Freelance.ModelOne.Server/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net;
+using System.Net.Sockets;
 using System.Threading;
 using NetMQ;
 
@@ -56,7 +57,7 @@ namespace Freelance.ModelOne.Server
 
             foreach (var ipAddress in ipEntry.AddressList)
             {
-                if (ipAddress.AddressFamily.ToString() == "InterNetwork")
+                if (ipAddress.AddressFamily == AddressFamily.InterNetwork)
                 {
                     return ipAddress.ToString();
                 }
