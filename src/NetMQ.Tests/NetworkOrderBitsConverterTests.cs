70a3b6cd684362e11e15d69d6d28ec0a6ec98552
Drew Noakes <git@drewnoakes.com>
2015-03-12 22:50:49 +0000

    Extend NetworkOrderBitsConverterTests.

--- src/NetMQ.Tests/NetworkOrderBitsConverterTests.cs
+++ src/NetMQ.Tests/NetworkOrderBitsConverterTests.cs
@@ -8,45 +8,53 @@ namespace NetMQ.Tests
         [Test]
         public void TestInt64()
         {
-            byte[] buffer =  NetworkOrderBitsConverter.GetBytes((long)1);
+            unchecked
+            {
+                RoundTripInt64(0x0102030405060708, 1, 2, 3, 4, 5, 6, 7, 8);
+                RoundTripInt64((long)0xFFEEDDCCBBAA9988, 0xFF, 0xEE, 0xDD, 0xCC, 0xBB, 0xAA, 0x99, 0x88);
+            }
+        }
 
-            Assert.AreEqual(buffer[7], 1);
-            Assert.AreEqual(0, buffer[0]);
+        private static void RoundTripInt64(long num, params byte[] bytes)
+        {
+            byte[] buffer = NetworkOrderBitsConverter.GetBytes(num);
 
-            long num = NetworkOrderBitsConverter.ToInt64(buffer);
+            Assert.AreEqual(8, buffer.Length);
+            CollectionAssert.AreEqual(bytes, buffer);
 
-            Assert.AreEqual(1, num);
+            Assert.AreEqual(num, NetworkOrderBitsConverter.ToInt64(buffer));
 
-            NetworkOrderBitsConverter.PutInt64(72057594037927936, buffer, 0);
+            NetworkOrderBitsConverter.PutInt64(num, buffer, 0);
 
-            Assert.AreEqual(1, buffer[0]);
-            Assert.AreEqual(0, buffer[7]);
+            CollectionAssert.AreEqual(bytes, buffer);
 
-            num = NetworkOrderBitsConverter.ToInt64(buffer);
-
-            Assert.AreEqual(72057594037927936, num);
+            Assert.AreEqual(num, NetworkOrderBitsConverter.ToInt64(buffer));
         }
 
         [Test]
         public void TestInt32()
         {
-            byte[] buffer = NetworkOrderBitsConverter.GetBytes(1);
+            unchecked
+            {
+                RoundTripInt32(0x01020304, 1, 2, 3, 4);
+                RoundTripInt32((int)0xFFEEDDCC, 0xFF, 0xEE, 0xDD, 0xCC);
+            }
+        }
 
-            Assert.AreEqual(1, buffer[3]);
-            Assert.AreEqual(0, buffer[0]);
+        private static void RoundTripInt32(int num, params byte[] bytes)
+        {
+            byte[] buffer = NetworkOrderBitsConverter.GetBytes(num);
 
-            long num = NetworkOrderBitsConverter.ToInt32(buffer);
+            Assert.AreEqual(4, buffer.Length);
+            CollectionAssert.AreEqual(bytes, buffer);
 
-            Assert.AreEqual(1, num);
+            Assert.AreEqual(num, NetworkOrderBitsConverter.ToInt32(buffer));
 
-            NetworkOrderBitsConverter.PutInt32(16777216, buffer,0);
+            NetworkOrderBitsConverter.PutInt32(num, buffer, 0);
 
-            Assert.AreEqual(1, buffer[0]);
-            Assert.AreEqual(0, buffer[3]);
+            CollectionAssert.AreEqual(bytes, buffer);
 
-            num = NetworkOrderBitsConverter.ToInt32(buffer);
-
-            Assert.AreEqual(16777216, num);
+            Assert.AreEqual(num, NetworkOrderBitsConverter.ToInt32(buffer));
         }
     }
 }
