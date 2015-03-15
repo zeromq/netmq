050e411cdc0d1cf86edff5c75603d83a2a218b22
Drew Noakes <git@drewnoakes.com>
2015-03-15 12:34:00 +0000

    Break 'Enums.cs' into two files, one for each enum within.

---
+++ src/NetMQ/zmq/ContextOption.cs
@@ -0,0 +1,11 @@
+﻿namespace NetMQ.zmq
+{
+    /// <summary>
+    /// This enum-type is either IOThreads (1) or MaxSockets (2).
+    /// </summary>
+    internal enum ContextOption
+    {
+        IOThreads = 1,
+        MaxSockets = 2
+    }
+}
--- src/NetMQ/zmq/ZmqSocketOption.cs
+++ src/NetMQ/zmq/ZmqSocketOption.cs
@@ -3,15 +3,6 @@
 namespace NetMQ.zmq
 {
     /// <summary>
-    /// This enum-type is either IOThreads (1) or MaxSockets (2).
-    /// </summary>
-    internal enum ContextOption
-    {
-        IOThreads = 1,
-        MaxSockets = 2
-    }
-
-    /// <summary>
     /// This enum-type serves to identity a particular socket-option.
     /// </summary>
     internal enum ZmqSocketOption
