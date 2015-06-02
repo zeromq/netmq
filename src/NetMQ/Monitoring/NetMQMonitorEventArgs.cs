3e6d262576e161b705439f0a296393f79f11dfe5
Drew Noakes <git@drewnoakes.com>
2015-06-02 23:37:59 +0100

    Documentation of NetMQMonitorEventArgs.

--- src/NetMQ/Monitoring/NetMQMonitorEventArgs.cs
+++ src/NetMQ/Monitoring/NetMQMonitorEventArgs.cs
@@ -5,7 +5,7 @@ using JetBrains.Annotations;
 namespace NetMQ.Monitoring
 {
     /// <summary>
-    /// This is an EventArgs that also contains a NetMQMonitor and a string Address.
+    /// Base class for all event arguments raised by <see cref="NetMQMonitor"/>.
     /// </summary>
     public abstract class NetMQMonitorEventArgs : EventArgs
     {
@@ -41,17 +41,17 @@ namespace NetMQ.Monitoring
     }
 
     /// <summary>
-    /// A NetMQMonitorSocketEventArgs is a subclass of NetMQMonitorEventArgs that also holds a socket.
+    /// A subclass of <see cref="NetMQMonitorEventArgs"/> that also holds a socket.
     /// </summary>
     public class NetMQMonitorSocketEventArgs : NetMQMonitorEventArgs
     {
         /// <summary>
         /// Create a new NetMQMonitorSocketEventArgs that contains the given monitor, address, and socket.
         /// </summary>
-        /// <param name="monitor">the NetMQMonitor that this event concerns</param>
-        /// <param name="address">a string denoting the endpoint-address</param>
-        /// <param name="socket">the AsyncSocket in question</param>
+        /// <param name="monitor">The <see cref="NetMQMonitor"/> that raised this event.</param>
+        /// <param name="address">The address of the event.</param>
         /// <param name="socketEvent">The type of socket event that occurred.</param>
+        /// <param name="socket">The socket upon which this event occurred.</param>
         public NetMQMonitorSocketEventArgs([NotNull] NetMQMonitor monitor, [NotNull] string address, [NotNull] AsyncSocket socket, SocketEvent socketEvent)
             : base(monitor, address, socketEvent)
         {
@@ -59,24 +59,24 @@ namespace NetMQ.Monitoring
         }
 
         /// <summary>
-        /// Get the AsyncSocket that this is holding.
+        /// Gets the socket upon which this event occurred.
         /// </summary>
         [NotNull]
         public AsyncSocket Socket { get; private set; }
     }
 
     /// <summary>
-    /// A NetMQMonitorErrorEventArgs is a subclass of NetMQMonitorEventArgs that also holds an ErrorCode.
+    /// A subclass of <see cref="NetMQMonitorEventArgs"/> that also holds an error code.
     /// </summary>
     public class NetMQMonitorErrorEventArgs : NetMQMonitorEventArgs
     {
         /// <summary>
         /// Create a new NetMQMonitorErrorEventArgs that contains the given monitor, address, and error-code.
         /// </summary>
-        /// <param name="monitor">the NetMQMonitor that this event concerns</param>
-        /// <param name="address">a string denoting the endpoint-address</param>
-        /// <param name="errorCode">the ErrorCode that is giving rise to this event</param>
+        /// <param name="monitor">The <see cref="NetMQMonitor"/> that raised this event.</param>
+        /// <param name="address">The address of the event.</param>
         /// <param name="socketEvent">The type of socket event that occurred.</param>
+        /// <param name="errorCode">The error code associated with this event.</param>
         public NetMQMonitorErrorEventArgs([NotNull] NetMQMonitor monitor, [NotNull] string address, ErrorCode errorCode, SocketEvent socketEvent)
             : base(monitor, address, socketEvent)
         {
@@ -84,22 +84,22 @@ namespace NetMQ.Monitoring
         }
 
         /// <summary>
-        /// Get the ErrorCode that this was constructed with.
+        /// Gets the error code associated with this event.
         /// </summary>
         public ErrorCode ErrorCode { get; private set; }
     }
 
     /// <summary>
-    /// A NetMQMonitorIntervalEventArgs is a subclass of NetMQMonitorEventArgs that also provides an Interval property to hold the reconnect-interval.
+    /// A subclass of <see cref="NetMQMonitorEventArgs"/> that also holds an interval.
     /// </summary>
     public class NetMQMonitorIntervalEventArgs : NetMQMonitorEventArgs
     {
         /// <summary>
-        /// Create a new NetMQMonitorIntervalEventArgs containing the given NetMQMonitor, address, and time-interval.
+        /// Create a new NetMQMonitorIntervalEventArgs containing the given NetMQMonitor, address, and interval.
         /// </summary>
         /// <param name="monitor">the NetMQMonitor</param>
-        /// <param name="address">a string denoting the address</param>
-        /// <param name="interval">the computed reconnect-interval</param>
+        /// <param name="address">The a string denoting the address</param>
+        /// <param name="interval">The interval, in milliseconds.</param>
         /// <param name="socketEvent">The type of socket event that occurred.</param>
         public NetMQMonitorIntervalEventArgs([NotNull] NetMQMonitor monitor, [NotNull] string address, int interval, SocketEvent socketEvent)
             : base(monitor, address, socketEvent)
@@ -108,7 +108,7 @@ namespace NetMQ.Monitoring
         }
 
         /// <summary>
-        /// Gets the computed reconnect interval.
+        /// Gets the interval, in milliseconds.
         /// </summary>
         public int Interval { get; private set; }
     }
