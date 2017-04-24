b17b533d579f53ca598d5604e36c7156a2720eac
Drew Noakes <git@drewnoakes.com>
2017-04-24 12:34:34 +0100

    Remove redundant static field initialiser.
    The default value is zero anyway.
    
    In the worst case, the compiler emits code on every usage of the type to
    ensure the type initializer has ran.

--- src/NetMQ/Sockets/PairSocket.cs
+++ src/NetMQ/Sockets/PairSocket.cs
@@ -8,7 +8,7 @@ namespace NetMQ.Sockets
     /// </summary>
     public class PairSocket : NetMQSocket
     {
-        private static int s_sequence = 0;
+        private static int s_sequence;
 
         /// <summary>
         /// Create a new PairSocket and attach socket to zero or more endpoints.
