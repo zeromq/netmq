554606e719ec20f6daf4c2861b11aeb6c02fc1a9
moreal <dev.moreal@gmail.com>
2019-03-19 16:58:52 +0900

    Complete to implement repair docs code

--- repair-docs.py
+++ repair-docs.py
@@ -2,26 +2,31 @@ import os
 
 DOCS_PATH = "./docs"
 REPAIR_DOCS_PATH = "./repair-docs"
-KEYWORD_LINE = "\t:::csharp"
 
 os.mkdir(REPAIR_DOCS_PATH)
 
 for docname in os.listdir("./docs"):
+    if "Images" == docname:
+        continue
+
     lines = open(f"{DOCS_PATH}/{docname}", 'r').readlines()
     new_doc = open(f"{REPAIR_DOCS_PATH}/{docname}", "w")
     flag = False
 
-    for line in lines:
-        if KEYWORD_LINE == line:
-            line = "``` csharp"
+    for index, line in enumerate(lines):
+        if line[4:7] == ":::":
+            line = f"``` {line.strip()[3:]}\n"
             flag = True
 
-        if flag and "" == line:
-            line = '```'
+        elif flag and ("\n" == line and "    " != lines[index+1][:4]):
+            line = '```\n\n'
             flag = False
 
-        new_doc.write(line+'\n')
+        elif flag:
+            line = line[4:]
+
+        new_doc.write(line)
 
 
-os.rmdir(DOCS_PATH)
-os.rename(REPAIR_DOCS_PATH, DOCS_PATH)
+# os.rmdir(DOCS_PATH)
+# os.rename(REPAIR_DOCS_PATH, DOCS_PATH)
