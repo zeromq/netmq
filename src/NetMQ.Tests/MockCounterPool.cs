a52276cbabbc64d05b6fe72d3656a743faca06b8
Ronald van der Velden <ronald.van.der.velden@gmail.com>
2020-03-18 09:44:04 +0100

    Updating tests

---
+++ src/NetMQ.Tests/MockCounterPool.cs
@@ -0,0 +1,37 @@
+﻿using NetMQ.Core.Utils;
+using System;
+
+namespace NetMQ.Tests
+{
+    public sealed class MockCounterPool : IAtomicCounterPool
+    {
+        public int TakeCallCount { get; private set; }        
+        public int ReturnCallCount { get; private set; }
+        
+        public MockCounterPool()
+        {
+            
+        }
+
+        public void Reset()
+        {
+            TakeCallCount = 0;
+            ReturnCallCount = 0;            
+        }
+
+        public AtomicCounter Take()
+        {
+            TakeCallCount++;
+            return new AtomicCounter();
+        }
+
+        public void Return(AtomicCounter counter)
+        {
+            ReturnCallCount++;
+        }
+
+        void IDisposable.Dispose()
+        {
+        }
+    }
+}
