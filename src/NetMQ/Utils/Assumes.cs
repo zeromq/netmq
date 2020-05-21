aa4ea848a68024b5e48cf3f6d3c1e7738f51b039
Drew Noakes <git@drewnoakes.com>
2020-05-21 20:13:19 +1000

    Add Assumes.NotNull
    Sometimes the compiler cannot know that a reference is not null. Using this method instructs it, and validates the fact at runtime in debug builds.

---
+++ src/NetMQ/Utils/Assumes.cs
@@ -0,0 +1,18 @@
+﻿#nullable enable
+
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace NetMQ
+{
+    internal static class Assumes
+    {
+#pragma warning disable CS8777 // Parameter must have a non-null value when exiting.
+        [Conditional("DEBUG")]
+        public static void NotNull<T>([NotNull] T o) where T : class?
+        {
+            Debug.Assert(o is object);
+        }
+#pragma warning restore CS8777 // Parameter must have a non-null value when exiting.
+    }
+}
