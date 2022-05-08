1264afb2ce60e33102a905b39a220965e8cfb4db
Shane Celis <shane.celis@gmail.com>
2022-05-08 05:01:32 -0400

    feature: Add ArrayBufferPool implementation.
    Add an ArrayPool<byte> from the System.Buffers namespace. With it, one
    can exclude the System.ServiceModel.Primitives package.

--- src/NetMQ/BufferPool.cs
+++ src/NetMQ/BufferPool.cs
@@ -1,6 +1,7 @@
 using System;
 using System.ServiceModel.Channels;
 using System.Threading;
+using System.Buffers;
 
 namespace NetMQ
 {    
@@ -24,6 +25,65 @@ namespace NetMQ
         void Return(byte[] buffer);
     }
 
+    public class ArrayBufferPool : IBufferPool
+    {
+        private readonly ArrayPool<byte> m_arrayPool;
+
+        /// <summary>
+        /// Create a new ArrayBufferPool with the specified maximum buffer pool size
+        /// and a maximum size for each individual buffer in the pool.
+        /// </summary>
+        /// <param name="maxBufferPoolSize">the maximum size to allow for the buffer pool</param>
+        /// <param name="maxBufferSize">the maximum size to allow for each individual buffer in the pool</param>
+        /// <exception cref="InsufficientMemoryException">There was insufficient memory to create the requested buffer pool.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Either maxBufferPoolSize or maxBufferSize was less than zero.</exception>
+        public ArrayBufferPool(long maxBufferPoolSize, int maxBufferSize)
+        {
+          m_arrayPool = ArrayPool<byte>.Create(maxBufferSize, (int) maxBufferPoolSize);
+        }
+
+        /// <summary>
+        /// Return a byte-array buffer of at least the specified size from the pool.
+        /// </summary>
+        /// <param name="size">the size in bytes of the requested buffer</param>
+        /// <returns>a byte-array that is the requested size</returns>
+        /// <exception cref="ArgumentOutOfRangeException">size cannot be less than zero</exception>
+        public byte[] Take(int size)
+        {
+            return m_arrayPool.Rent(size);
+        }
+
+        /// <summary>
+        /// Return the given buffer to this manager pool.
+        /// </summary>
+        /// <param name="buffer">a reference to the buffer being returned</param>
+        /// <exception cref="ArgumentException">the Length of buffer does not match the pool's buffer length property</exception>
+        /// <exception cref="ArgumentNullException">the buffer reference cannot be null</exception>
+        public void Return(byte[] buffer)
+        {
+            m_arrayPool.Return(buffer);
+        }
+
+        /// <summary>
+        /// Release the buffers currently cached in this manager.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Release the buffers currently cached in this manager.
+        /// </summary>
+        /// <param name="disposing">true if managed resources are to be disposed</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposing)
+                return;
+            // We don't have a means of clearing ArrayPool like BufferManager does.
+        }
+    }
     /// <summary>
     /// This implementation of <see cref="IBufferPool"/> uses WCF's <see cref="BufferManager"/>
     /// class to manage a pool of buffers.
