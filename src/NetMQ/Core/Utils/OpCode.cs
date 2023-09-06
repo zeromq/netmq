6e9e93368228f3598e5587ad30d014467313d7e3
rickshaw5724 <rick.william.shaw@gmail.com>
2023-09-06 18:28:37 -0400

    Altered the environment variable read to accept any value, rather than just "true". Added a reference to the github issue.

--- src/NetMQ/Core/Utils/OpCode.cs
+++ src/NetMQ/Core/Utils/OpCode.cs
@@ -11,13 +11,14 @@ namespace NetMQ.Core.Utils
 
         public static bool Open()
         {
-            //Look for an environment variable: "NETQM_SUPPRESS_RDTSC" with a value of "TRUE"
+            //Look for an environment variable: "NETQM_SUPPRESS_RDTSC" with any value
             // The application can set this environment variable when this code is running in a system where
             // it is not desirable to read the processor's time stamp counter.
             // While this is supported in modern CPUs, the technique used for allocating executable memory, copying OP Code
             // for the read of the time stamp and invoking the OP Code can be detected as Malware by some anti-virus vendors.
-            var val = Environment.GetEnvironmentVariable("NETQM_SUPPRESS_RDTSC");
-            if ("TRUE".Equals(val, StringComparison.OrdinalIgnoreCase))
+            // https://github.com/zeromq/netmq/issues/1071
+            string val = Environment.GetEnvironmentVariable("NETQM_SUPPRESS_RDTSC");
+            if (!string.IsNullOrEmpty(val))
                 return false;
 #if NETSTANDARD1_1_OR_GREATER || NET471_OR_GREATER
             if (RuntimeInformation.ProcessArchitecture != Architecture.X86 &&
